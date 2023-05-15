22891c5a5e6d1ce15518a7d21fc0ec3f40585c44
ArekWaa <72909866+ArekWaa@users.noreply.github.com>
2023-05-15 18:08:53 +0200

    wyswietlanie piosenek, obsluga gornych przyciskow

--- app/src/main/java/com/example/songs/MainActivity.kt
+++ app/src/main/java/com/example/songs/MainActivity.kt
@@ -1,141 +1,103 @@
 package com.example.songs
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.Button
+import android.widget.HorizontalScrollView
+import android.widget.ImageButton
+import android.widget.ScrollView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var sizeButton: Button
+    private lateinit var scrollButton: ImageButton
+    private lateinit var scrollView: ScrollView
+    private lateinit var speedButton: Button
+    private lateinit var backgroundButton: ImageButton
+    private val lyricsList = mutableListOf<String>()
+    private val fontList = listOf(18, 24, 32)
+    private var selectedFont = 0
+    private var isScrolling = false
+    private lateinit var scrollAnimation: ObjectAnimator
+    private val speedList = listOf(0.5, 1.0, 2.0)
+    private var selectedSpeed = 1;
+    private val colorList = listOf(R.color.white, R.color.grey1, R.color.grey2)
+    private var selectedColor = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+        sizeButton = findViewById(R.id.size_button)
+        scrollButton = findViewById(R.id.scroll_button)
+        scrollView = findViewById(R.id.scrollView)
+        speedButton = findViewById(R.id.speeed_button)
+        backgroundButton = findViewById(R.id.bacground_button)
+
+        lyricsList.add(getString(R.string.song1))
+
+        loadSong(0)
+        setFontSize(fontList[0])
+        setBackgroundColor()
+
+        sizeButton.setOnClickListener{onChangeSizeButtonClick()}
+        scrollButton.setOnClickListener {
+            onScrollButtonClick()
+        }
+
+        speedButton.setOnClickListener{onSpeedButtonClick()}
+        backgroundButton.setOnClickListener{onColorButtonClick()}
+
     }
 
-    private val lyricsList = listOf(
-        "God of Jacob, Great I Am\n" +
-                "King of Angels, Son of Man\n" +
-                "Voice of many waters\n" +
-                "Song of Heaven's throne\n" +
-                "Louder than the thunder\n" +
-                "Make Your glory known\n" +
-                "\n" +
-                "[Chorus: Chris Brown]\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "\n" +
-                "[Post-Chorus: Brandon Lake & Chris Brown]\n" +
-                "Roar (Roar)\n" +
-                "Roar (Roar)\n" +
-                "Roar\n" +
-                "\n" +
-                "[Verse 2: Chris Brown]\n" +
-                "Pride of Zion, prophets spoke\n" +
-                "Our Messiah, flesh and bone\n" +
-                "You alone are worthy\n" +
-                "To open up the scroll\n" +
-                "Like the lamb, You suffered\n" +
-                "But the Lion has a roar\n" +
-                "\n" +
-                "[Chorus: Chris Brown & Brandon Lake]\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "\n" +
-                "[Post-Chorus: Brandon Lake & Chris Brown]\n" +
-                "Roar (Roar)\n" +
-                "Roar (Roar)\n" +
-                "Roar (Roar)\n" +
-                "Oh, lеt it roar, let it roar, let it roar\n" +
-                "Let thе Lion roar\n" +
-                "\n" +
-                "[Bridge: Brandon Lake, Chris Brown & Tiffany Hudson]\n" +
-                "Prepare the way\n" +
-                "Prepare the way of the Lord\n" +
-                "Prepare the way\n" +
-                "Prepare the way of the Lord\n" +
-                "Prepare the way\n" +
-                "Prepare the way of the Lord\n" +
-                "Prepare the way\n" +
-                "Prepare the way of the Lord\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "\n" +
-                "[Post-Bridge: Brandon Lake]\n" +
-                "Roar (Roar)\n" +
-                "Roar (Roar)\n" +
-                "Let the Lion roar (Roar)\n" +
-                "Roar (Roar)\n" +
-                "\n" +
-                "[Chorus: Chris Brown & Brandon Lake, Brandon Lake]\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar (Let the Lion roar)\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "Hail, hail, Lion of Judah\n" +
-                "Let the Lion roar\n" +
-                "\n" +
-                "[Bridge: Brandon Lake & Tiffany Hudson, Chris Brown]\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "Oh, valley (Prepare the way)\n" +
-                "Be raised up (Prepare the way of the Lord)\n" +
-                "Oh, mountain (Prepare the way)\n" +
-                "Be made low (Prepare the way of the Lord)\n" +
-                "\n" +
-                "[Outro: Chris Brown & Brandon Lake]\n" +
-                "Oh, valley be raised up\n" +
-                "Oh, mountain be made low\n" +
-                "Oh, valley be raised up\n" +
-                "Oh, mountain be made low",
-        "Tekst 222222",
-        "Tekst 3333333"
-    )
+    private fun loadSong(id: Int)
+    {
+        textView.text = lyricsList[id]
+    }
 
+    private fun onChangeSizeButtonClick()
+    {
+        selectedFont = (selectedFont + 1) % fontList.size
+        setFontSize(fontList[selectedFont])
+    }
+
+    private fun setFontSize(size: Int)
+    {
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, size.toFloat())
+    }
+
+    private fun onScrollButtonClick()
+    {
+        isScrolling = !isScrolling
+        if (isScrolling)
+        {
+            scrollAnimation = ObjectAnimator.ofInt(scrollView, "scrollY", scrollView.getChildAt(0).height)
+            scrollAnimation.setDuration((1 / speedList[selectedSpeed] * 1 * 60 * 1000).toLong())
+            scrollAnimation.start()
+        }
+        else
+        {
+            scrollAnimation.cancel()
+        }
+    }
+
+    private fun onSpeedButtonClick()
+    {
+        selectedSpeed = (selectedSpeed + 1) % speedList.size
+        speedButton.text = speedList[selectedSpeed].toString()
+    }
+
+    private fun onColorButtonClick()
+    {
+        selectedColor = (selectedColor + 1) % colorList.size
+        setBackgroundColor()
+    }
+
+    private fun setBackgroundColor()
+    {
+        textView.setBackgroundResource(colorList[selectedColor])
+    }
 }
